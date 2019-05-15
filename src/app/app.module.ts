ee1fbbca58a9946315a58f77edf4ef046bb5bed6
KaMa75 <karolmarszalek@o2.pl>
2019-05-15 13:25:56 +0200

    New components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { DetailsContactComponent } from './details-contact/details-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    AddContactComponent,
+    EditContactComponent,
+    DetailsContactComponent
   ],
   imports: [
     BrowserModule,
