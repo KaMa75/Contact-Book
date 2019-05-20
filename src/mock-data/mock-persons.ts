72955f288f5e6b7d6e0fa317253a83733f66fcb6
KaMa75 <karolmarszalek@o2.pl>
2019-05-20 21:46:01 +0200

    fix example dat in db

--- src/mock-data/mock-persons.ts
+++ src/mock-data/mock-persons.ts
@@ -7,8 +7,8 @@ export const personsData: Array<Person> = [
     firstName: 'Karol',
     lastName: 'Marszałek',
     email: 'karolmarszalek@o2.pl',
-    phone: '+48 604 753 086',
-    sex: 'male'
+    phone: '+48604753086',
+    sex: 'Mężczyzna'
   },
   {
     id: 2,
@@ -16,8 +16,8 @@ export const personsData: Array<Person> = [
     firstName: 'Cezar',
     lastName: 'Grabowski',
     email: 'CezarGrabowski@teleworm.us',
-    phone: '+48 51 106 09 15',
-    sex: 'male'
+    phone: '+48511060915',
+    sex: 'Mężczyzna'
   },
   {
     id: 3,
@@ -25,7 +25,7 @@ export const personsData: Array<Person> = [
     firstName: 'Franciszka',
     lastName: 'Sobczak',
     email: 'FranciszkaSobczak@armyspy.com',
-    phone: '+48 88 347 42 37',
-    sex: 'female'
+    phone: '+48883474237',
+    sex: 'Kobieta'
   }
 ];
