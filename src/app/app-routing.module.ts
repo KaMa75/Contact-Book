7ab673ce2b13ee14eb6ab1d03e7c30bab98d5847
KaMa75 <karolmarszalek@o2.pl>
2019-05-23 15:53:09 +0200

    Remove unused route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactListComponent } from './contact-list/contact-list.component';
-import { AddContactComponent } from './add-contact/add-contact.component';
 import { DetailsContactComponent } from './details-contact/details-contact.component';
 import { EditContactComponent } from './edit-contact/edit-contact.component';
 
@@ -10,10 +9,6 @@ const routes: Routes = [
   {
     path: '',
     component: ContactListComponent
-  },
-  {
-    path: 'add',
-    component: AddContactComponent
   },
   {
     path: 'clients/details/:id',
